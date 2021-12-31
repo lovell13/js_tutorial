d2cebb11ce08bed17dfb234d1113e58cd1a68acb
Troy <alcinov@gmail.com>
2021-12-31 00:29:31 -0400

    Finish the word counter script

--- count.js
+++ count.js
@@ -1,4 +1,3 @@
-
 const sonnet = `Let me not to the marriage of true minds
 Admit impediments. Love is not love
 Which alters when it alteration finds,
@@ -14,10 +13,22 @@ But bears it out even to the edge of doom.
 If this be error and upon me proved,
 I never writ, nor no man ever loved.`;
 
+// Unique words
+let uniques = {};
+// All words in the text
+let words = sonnet.match(/\w+/g);
 
+// Iterate through `words` and build up an associative array of unique words.
+for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (uniques[word]) {
+        uniques[word] += 1;
+    } else {
+        uniques[word] = 1;
+    }
+}
 
-
-
+console.log(uniques)
 
 
 
