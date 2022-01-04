69f536e7859a5530d2cc0f37fd28e8cbf56520b6
Troy <alcinov@gmail.com>
2022-01-03 22:56:09 -0400

    Completed Map exercise in functional programming

--- functional.js
+++ functional.js
@@ -1,3 +1,31 @@
+let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
+// Returns a URL-friendly version of a string.
+// Example: "North Dakota" -> "north-dakota"
+function urlify(string) {
+    return string.toLowerCase().split(/\s+/).join("-");
+}
 
+// urls: Imperative version
+function imperativeUrls(elements) {
+    let urls = [];
+    elements.forEach(function(element) {
+        urls.push(urlify(element));
+    });
+    return urls;
+}
+console.log(imperativeUrls(states));
 
+// urls: Functional version
+function functionalUrls(elements) {
+    return elements.map(element => urlify(element));
+}
+console.log(functionalUrls(states));
+
+// makeUrl function - take in the state variable, return an array of URLs.
+function makeUrl(inputs) {
+    let urlTemplate = "https://example.com/";
+    let fullUrls = [];
+    return inputs.map(inputs => urlTemplate.concat(inputs));
+}
+console.log(makeUrl(functionalUrls(states)));
